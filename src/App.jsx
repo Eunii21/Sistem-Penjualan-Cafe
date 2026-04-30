3c7a9ba3b3919b2e3247d6610e42b423312c7d67
Dhoo365 <ridhowael97@gmail.com>
2026-04-30 22:42:05 +0800

    tambah landing page, tambah react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-import Background from "./assets/MacBook Air - 9.png" 
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage"
 
 function App() {
   return (
-    <div
-      className="h-screen w-full bg-cover bg-center flex items-center justify-center relative"
-      style={{ backgroundImage: `url(${Background})` }}
-    >
-      {/* overlay */}
-      <div className="absolute inset-0 bg-[#3b1f1a]/70"></div>
-
-      {/* content */}
-      <div className="relative z-10 text-center text-white">
-        <h1 className="text-4xl md:text-5xl font-abhaya font-extrabold mb-6">
-          Mesombang Cafe
-        </h1>
-
-        <button className="px-8 py-3 bg-[#b08980] hover:bg-[#8c6b63] transition rounded-full text-lg font-medium shadow-lg font-abhaya">
-          mulai
-        </button>
-      </div>
-    </div>
+   <Routes>
+   <Route path="/landing-page" element={<LandingPage />}/>
+   </Routes>
   );
 }
 
